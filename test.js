e241c284c92ec226746c7f454553e542a6731429
Suraj Mishra <mishrasur7@gmail.com>
2021-03-05 12:09:15 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,6 @@
+function showGreetings() {
+    document.getElementById("pOutput").innerHTML = "Hello Sir, good to see you here"; 
+}
+function showDateAndTime () {
+    document.getElementById("pOutPut").innerHTML = "You have a following date and time in your location " + "<br />" + Date(); 
+}
