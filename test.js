516bfacbb5873d385366090a49776b6bc5f1908b
Suraj Mishra <mishrasur7@gmail.com>
2021-03-07 07:43:45 +0200

    light turn and off added

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 function showGreetings() {
     document.getElementById("pOutput").innerHTML = "Hello Sir, good to see you here"; 
 }
+
 function showDateAndTime () {
     document.getElementById("pOutPut").innerHTML = "You have a following date and time in your location " + "<br />" + Date(); 
 }
+
 function showYourEmail() {
     var email = document.getElementById("textEmail").value; 
     if (isNaN(email) === false) {
@@ -16,4 +18,12 @@ function showPriceWithTax() {
     var price = Number(document.getElementById("price").value); 
     var priceWithTax = price + price * 0.24; 
     document.getElementById("withTax").innerHTML = "Final price with tax is " + priceWithTax.toFixed(2) + " euros !"; 
+}
+
+function turnOnTheLight() {
+    document.getElementById("myImage").src = "images/bulbon.gif"; 
+}
+
+function turnOffTheLight() {
+    document.getElementById("myImage").src = "images/bulboff.gif"; 
 }
