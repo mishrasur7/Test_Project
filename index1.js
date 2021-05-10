abf7eee14ad172a884ee9872ff6eed77ca5ed9de
Suraj Mishra <mishrasur7@gmail.com>
2021-05-10 18:07:18 +0300

    more tests

--- index1.js
+++ index1.js
@@ -1,7 +1,27 @@
  
 
-for(var i = 0; i <= 20; i++) {
+for(var i = 0; i < 1; i++) {
     console.log("Suraj"); 
 }
 
 
+for(var i = 0; i <= 10; i++) {
+    var randomNum = Math.floor(Math.random() * 51 + 200)
+    console.log(randomNum); 
+}
+
+
+var players = ["suraj", "shova", "sujan", "ram"]; 
+var socres = [10, 34, 15, 11]; 
+
+//finding the total scores of all players 
+
+var sum = 0; 
+
+for(var i = 0; i < socres.length; i++) {
+    sum += socres[i]; 
+}
+
+console.log("The total sum is " + sum); 
+
+//finding the maximum score and the player with maximum score
