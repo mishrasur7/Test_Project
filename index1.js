70fdc585ecf799d9d33a869579e2ab917dd7bc2d
Suraj Mishra <mishrasur7@gmail.com>
2021-05-10 18:42:58 +0300

    more tests

--- index1.js
+++ index1.js
@@ -12,16 +12,33 @@ for(var i = 0; i <= 10; i++) {
 
 
 var players = ["suraj", "shova", "sujan", "ram"]; 
-var socres = [10, 34, 15, 11]; 
+var scores = [10, 34, 15, 11]; 
 
 //finding the total scores of all players 
 
 var sum = 0; 
 
-for(var i = 0; i < socres.length; i++) {
-    sum += socres[i]; 
+for(var i = 0; i < scores.length; i++) {
+    sum += scores[i]; 
 }
 
 console.log("The total sum is " + sum); 
 
-//finding the maximum score and the player with maximum score
+//finding the maximum score and the player name with maximum score
+
+var maxSoFar = scores[0]; 
+for(var i = 0; i < scores.length; i++) {
+    if(scores[i] > maxSoFar) {
+        maxSoFar = scores[i]; 
+    }
+}
+ var outPutText; 
+ for(var i = 0; i < players.length; i++) {
+     if(maxSoFar === scores[i]) {
+         outPutText = players[i] + " has the maximum score " + maxSoFar;  
+     }
+ }
+ console.log(outPutText); 
+
+ //Finding the minimum score and the player name with minimum score
+ 
