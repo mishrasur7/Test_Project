bed89398ea5acf5bfd5d5c11e039dbf23cd26123
Suraj Mishra <mishrasur7@gmail.com>
2021-05-10 22:48:33 +0300

    more tests added

--- index1.js
+++ index1.js
@@ -12,7 +12,7 @@ for(var i = 0; i <= 10; i++) {
 
 
 var players = ["suraj", "shova", "sujan", "ram"]; 
-var scores = [10, 34, 15, 11]; 
+var scores = [107, 34, 15, 113]; 
 
 //finding the total scores of all players 
 
@@ -64,3 +64,44 @@ for(var i = 0; i < scores.length; i++) {
  console.log("Average score of all players is " + average.toFixed(2)); 
 
  //finding the second highest score 
+
+
+ function secondBiggest() {
+     if(scores.length >= 1) {
+         var maxSoFar = scores[0]; 
+         var secondBiggestSoFar = undefined; 
+
+         function updateIfBigger(newValue) {
+             if((secondBiggestSoFar === undefined && newValue !== maxSoFar) || newValue > secondBiggestSoFar) {
+                 secondBiggestSoFar = newValue; 
+             }
+         }
+        for(var i = 0; i < scores.length; i++) {
+            if(scores[i] > maxSoFar) {
+                var oldMaxSoFar = maxSoFar; 
+                maxSoFar = scores[i]; 
+                updateIfBigger(oldMaxSoFar); 
+            } else if(scores[i] < maxSoFar) {
+                updateIfBigger(scores[i]); 
+            }
+        }
+
+        var winnerNames = "The winners are: "; 
+        for(var i = 0; i < scores.length; i++) {
+            if(scores[i] === maxSoFar) {
+                winnerNames += players[i] + ":" + scores[i] + " points"; 
+            }
+        }
+        if(secondBiggestSoFar !== undefined) {
+            var secondPlacerNames = "The second placers are: " ; 
+            for(var i = 0; i < scores.length; i++) {
+                if(scores[i] === secondBiggestSoFar) {
+                    secondPlacerNames += players[i] + ":" + scores[i] + " points"; 
+                }
+            }
+        }
+        document.getElementById("result").innerHTML = winnerNames + "<br />" + secondPlacerNames; 
+     } else {
+        document.getElementById("result").innerHTML = "Error: Empty array!";  
+     }
+ }
