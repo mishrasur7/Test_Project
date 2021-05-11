dfebbc72bef50ae44dfa38594ea10df23af96fee
Suraj Mishra <mishrasur7@gmail.com>
2021-05-11 23:43:11 +0300

    more tests

--- index3.js
+++ index3.js
@@ -1,20 +1,78 @@
 var players = ["suraj", "ram", "sujan", "shova", "alpo"]; 
-var scores = [10, 10, 30, 10, 5]; 
+var scores = [10, 19, 30, 10, 5]; 
+
+var result = ""; 
+for(var i = 0; i < scores.length; i++) {
+    if(scores[i] > 10) {
+        result += players[i] + " score: " + scores[i] + " "; 
+    }
+}
+console.log(result); 
+
 
 //finding the minimum scores in the array 
 var minSoFar = scores[0]; 
-var outPutText = "The players having minimum scores are: "; 
+var secondMin = undefined; 
+
+function updateIfSmaller(newValue) {
+    if((secondMin === undefined && newValue !== minSoFar) || newValue > secondMin) {
+        secondMin = newValue; 
+        return secondMin; 
+    }
+}
 
 for(var i = 0; i < scores.length; i++) {
     if(scores[i] < minSoFar) {
+        var oldMinSoFar = minSoFar; 
         minSoFar = scores[i]; 
+        secondMin = updateIfSmaller(oldMinSoFar); 
+    } else if(scores[i] > minSoFar) {
+        secondMin = updateIfSmaller(scores[i]); 
     }
 }
 
+var firstLooser = "First looser is "; 
 for(var i = 0; i < scores.length; i++) {
     if(scores[i] === minSoFar) {
-        outPutText += players[i] + " " + scores[i] + " scores." + "<br />"; 
+        firstLooser += players[i] + " score: " + scores[i]; 
     }
 }
 
-document.getElementById("output").innerHTML = outPutText; 
+var secondLooser = "Second looser is "; 
+if(secondMin !== undefined) {
+    for(var i = 0; i < scores.length; i++) {
+        if(scores[i] === secondMin) {
+            secondLooser += players[i] + " score: " + scores[i]; 
+        }
+    }
+}
+document.getElementById("output").innerHTML = firstLooser + "<br />" + secondLooser; 
+
+////////////////////////////////////////////////////////
+
+
+var a = [1, 2, 3, 4];
+var sum = 0; 
+for(var i = 0; i < a.length; i++) {
+    sum += a[i]; 
+}
+////////////////////////////////////////////////////////
+var average = sum/a.length; 
+
+console.log(average); 
+
+function calculateSalary() {
+var hourInput = document.getElementById("hour").value; 
+if(hourInput.trim() === "" || isNaN(hourInput)) {
+    alert("Please put your hours!"); 
+} else {
+    var hour = Number(hourInput);
+    var totalSalary;  
+    if(hour <= 8) {
+        totalSalary = hour * 10; 
+    } else {
+        totalSalary = 80 + (hour-8) * 15; 
+    }
+    console.log(totalSalary); 
+}
+}
